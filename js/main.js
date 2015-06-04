f882bb3eb894504949db3b3baad5a1653ce88cdb
DP <Byte@DPs-MacBook-Air.local>
2015-06-03 20:27:17 -0400

    updated main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,35 @@
+$('button').on('click', function() {
+
+	if ( $(this).hasClass('on') ) {
+
+		$(this).removeClass('on').addClass('off');
+
+		$('.status').text('Too dark!');
+
+		$('body').removeClass('light').addClass('dark');
+
+	}
+
+	else if ( $(this).hasClass('off') ) {
+
+		$(this).removeClass('off').addClass('on');
+
+		$('.status').text('Too bright!');
+
+		$('body').removeClass('dark').addClass('light');
+
+	}
+
+});
+
+
+
+
+
+
+
+
+
+
 
 
